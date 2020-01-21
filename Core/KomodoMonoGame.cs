27ffe96545957f113ce5c418de2938d13480da84
James Orson <jamesaorson@gmail.com>
2020-01-20 21:31:15 -0800

    OLD-REPO: Abstract out base MonoGame components

---
+++ Core/KomodoMonoGame.cs
@@ -0,0 +1,71 @@
+﻿using Komodo.Core.Graphics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Komodo.Core
+{
+    internal class KomodoMonoGame : Microsoft.Xna.Framework.Game
+    {
+        #region Members
+
+        #region Public Members
+        public SpriteBatch _spriteBatch;
+        #endregion Public Members
+
+        #region Protected Members
+        #endregion Protected Members
+
+        #region Private Members
+        private IKomodoGame _komodoGame;
+        #endregion Private Members
+
+        #endregion Members
+
+        #region Member Methods
+        
+        #region Public Member Methods
+        #endregion Public Member Methods
+        
+        #region Protected Member Methods
+        protected override void Draw(GameTime gameTime)
+        {
+            _komodoGame.Draw(gameTime, Color.Red);
+
+            base.Draw(gameTime);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _komodoGame.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+        #endregion Protected Member Methods
+        
+        #region Private Member Methods
+        #endregion Private Member Methods
+
+        #endregion Member Methods
+
+        #region Constructors
+        public KomodoMonoGame(IKomodoGame komodoGame)
+        {
+            _komodoGame = komodoGame;
+            Content.RootDirectory = "Content/MonoGame";
+            IsMouseVisible = true;
+        }
+        #endregion Constructors
+    }
+}
