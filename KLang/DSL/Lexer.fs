91db0ccbaf0505cce8c628e486a64ce5db2a1ce9
James Orson <jamesaorson@gmail.com>
2020-04-08 23:25:18 -0700

    KOMODO-77: Create initial Lexer module

---
+++ KLang/DSL/Lexer.fs
@@ -0,0 +1,15 @@
+﻿module DSL.Lexer
+
+open System.IO
+
+let Lex source =
+    printfn "Source: %s" source
+    source.Split Array.empty
+    |> Array.toList
+
+let LexFile filePath =
+    if File.Exists filePath then
+        let source = File.ReadAllText filePath
+        Lex source
+    else
+        []
