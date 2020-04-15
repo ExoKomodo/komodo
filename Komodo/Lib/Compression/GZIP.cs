7766551968c9f508a58bb981ff6f598e2d038707
JamesOrson <james.osborne112@gmail.com>
2020-04-15 15:58:40 -0700

    KOMODO-97: Small refactor following VS analysis

--- Komodo/Lib/Compression/GZIP.cs
+++ Komodo/Lib/Compression/GZIP.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Komodo.Lib.Compression
 {
-    public class GZIP
+    public static class GZIP
     {
 
         #region Static Methods
@@ -13,42 +13,30 @@ namespace Komodo.Lib.Compression
         public static byte[] Compress(string data) => Compress(Encoding.ASCII.GetBytes(data));
         public static byte[] Compress(byte[] data)
         {
-            using (var compressedStream = new MemoryStream())
+            using var compressedStream = new MemoryStream();
+            using (var decompressedStream = new MemoryStream(data))
             {
-                using (var decompressedStream = new MemoryStream(data))
-                {
-                    using (
-                        var gzip = new GZipStream(
-                            compressedStream,
-                            CompressionMode.Compress
-                        )
-                    )
-                    {
-                        decompressedStream.CopyTo(gzip);
-                    }
-                }
-                return compressedStream.ToArray();
+                using var gzip = new GZipStream(
+                        compressedStream,
+                        CompressionMode.Compress
+                    );
+                decompressedStream.CopyTo(gzip);
             }
+            return compressedStream.ToArray();
         }
 
         public static byte[] Decompress(byte[] data)
         {
-            using (var decompressedStream = new MemoryStream())
+            using var decompressedStream = new MemoryStream();
+            using (var compressedStream = new MemoryStream(data))
             {
-                using (var compressedStream = new MemoryStream(data))
-                {
-                    using (
-                        var gzip = new GZipStream(
-                            compressedStream,
-                            CompressionMode.Decompress
-                        )
-                    )
-                    {
-                        gzip.CopyTo(decompressedStream);
-                    }
-                }
-                return decompressedStream.ToArray();
+                using var gzip = new GZipStream(
+                        compressedStream,
+                        CompressionMode.Decompress
+                    );
+                gzip.CopyTo(decompressedStream);
             }
+            return decompressedStream.ToArray();
         }
         
         #endregion Public Static Methods
